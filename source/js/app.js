0ebcc964e25c7b44c70c99af1ed86cc8489a38d1
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-06-17 22:45:28 +0200

    add dom animations with scrollreveal.js

--- source/js/app.js
+++ source/js/app.js
@@ -2,3 +2,45 @@ function showNav() {
     var x = document.querySelector("#responsive-nav");
     x.classList.toggle('unfold');
 }
+
+window.sr = ScrollReveal();
+sr.reveal('#header img.phone', {
+    duration: 300,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+sr.reveal('#features div.car', {
+    delay: 200,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#features div.shoe', {
+    delay: 400,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#features div.device', {
+    delay: 600,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#discount .offer', {
+    delay: 500,
+    duration: 300,
+    origin: 'right',
+    distance: '200px'
+});
+
+sr.reveal('#discount img', {
+    delay: 500,
+    duration: 300,
+    origin: 'left',
+    distance: '200px'
+});
